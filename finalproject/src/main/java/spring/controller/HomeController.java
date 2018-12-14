7504a5c435e693aee9009946b8908963b9401f77
RB <RB@RB-PC>
2018-12-14 14:05:18 +0900

    12/14 java [insert] configuration - DatabaseConfiguration, MybatisConfiguration controller - MemberController entity - MemberDto repository - MemberDao, MemberDaoEx
    xml
    [insert]
    mybatis - mybatis, member-mapper, db.properties
    [change]
    spring - appliction-config
    
    jsp
    [insert]
    member file - login, regist
    [change]
    home
    
    pom
    [change]
    Mybatis (dependency)

--- finalproject/src/main/java/spring/controller/HomeController.java
+++ finalproject/src/main/java/spring/controller/HomeController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
 	@RequestMapping("/home")
 	public String home() {
-		return /*/WEB-INF/jsp/*/"home"/*.jsp*/;
+		return "home";
 	}
-	
 }
--- finalproject/src/main/webapp/WEB-INF/view/home.jsp
+++ finalproject/src/main/webapp/WEB-INF/view/home.jsp
@@ -1,3 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
     
 <h1>home.jsp</h1>
+
+<h3><a href="member/regist">회원가입</a></h3>
+<h3><a href="member/login">로그인</a></h3>
